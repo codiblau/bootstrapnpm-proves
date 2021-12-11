814ddd5b4922596dab02ccdf55672b7cbd389d91
joangalmesriera <joan.galmes@gmail.com>
2021-12-11 08:16:58 +0100

    ff

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-import { } from '../node_modules/bootstrap'
+import { } from './bootstrap'
 
 var myCarousel = document.querySelector('#carouselIESManacor')
 var carousel = new bootstrap.Carousel(myCarousel, {
